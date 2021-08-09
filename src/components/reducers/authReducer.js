2e710d71655c744646332cc569270b65a653703a
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-09 18:29:37 +0500

    update auth reducer

--- src/components/reducers/authReducer.js
+++ src/components/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import {SIGN_IN, SIGN_OUT, SIGN_UP} from '../actions/types';
 const INITIAL_STATE={
     isSignedIn: null,
-    user: null,
+    userEmail: null,
     accessToken: null,
     instaUserId:null,
 };
@@ -10,7 +10,7 @@ switch (action.type){
     case SIGN_IN:
         return {...state, accessToken: action.payload.access_token, instaUserId: action.payload.userId};
     case SIGN_UP:
-        return {...state, isSignedIn: true, user: action.payload};
+        return {...state, isSignedIn: true, userEmail: action.payload};
     case SIGN_OUT:
         return {...state, isSignedIn: false, userId: null, accessToken: null};
     default:
