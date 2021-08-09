a7ae858a09c50cdd19885a186c5e6c89c8433e02
Umair Khalid <53624804+Umair599@users.noreply.github.com>
2021-08-09 16:55:22 +0500

    update types

--- src/components/actions/types.js
+++ src/components/actions/types.js
@@ -1,4 +1,4 @@
 export const SIGN_IN='SIGN_IN';
 export const SIGN_OUT='SIGN_OUT';
-export const ACCESS_TOKEN='ACCESS_TOKEN';
+export const SIGN_UP='SIGN_UP';
 export const FETCH_POSTS='FETCH_POSTS';
